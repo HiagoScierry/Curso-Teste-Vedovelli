c43d929529d9caa153ad99fa8a381c64ed8367fc
Hiago Moreira <47890178+HiagoScierry@users.noreply.github.com>
2023-04-30 19:10:20 -0300

    refact discounts

--- introdução/src/lib/Cart.js
+++ introdução/src/lib/Cart.js
@@ -1,42 +1,11 @@
 import { find, remove, throttle } from 'lodash';
+import { calculateDiscount } from '../utils/discounts';
 import Dinero from 'dinero.js';
 
 const Money = Dinero
 
 Money.defaultCurrency = 'BRL';
 Money.defaultPrecision = 2;
-
-const calculatePercentageDiscount = (amount, { condition, quantity }) => {
-    if (condition?.percentage && quantity > condition.minimum) {
-        return amount.percentage(condition.percentage);
-    };
-    return Money({ amount: 0 });
-};
-
-const calculateQuantityDiscount = (amount, { quantity, condition }) => {
-    const isEven = quantity % 2 === 0;
-
-    if (condition?.quantity && quantity > condition.quantity) {
-        return amount.percentage(isEven ? 50 : 40);
-    };
-    return Money({ amount: 0 });
-};
-
-const calculateDiscount = (amount, quantity, condition) => {
-    const list = Array.isArray(condition) ? condition : [condition];
-
-    const [higherDiscount] = list.map((cond) => {
-        if (cond.percentage) {
-            return calculatePercentageDiscount(amount, { quantity, condition: cond }).getAmount();
-        } else if (cond.quantity) {
-            return calculateQuantityDiscount(amount, { quantity, condition: cond }).getAmount();
-        }
-    }).sort((a, b) => b - a);
-
-    return Money({ amount: higherDiscount });
-
-}
-
 
 export default class Cart {
     items = [];
